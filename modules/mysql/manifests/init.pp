b66dda1e0e4d4b82bd1bcd5032425c7f7f9712ab
David Joy <davjoy@gmail.com>
2014-05-06 10:14:55 -0400

    Adding a default password if a user wants to use the local db.

--- modules/mysql/manifests/init.pp
+++ modules/mysql/manifests/init.pp
@@ -1,44 +1,44 @@
-class mysql 
+class mysql
 {
-    $mysqlPassword = "newpassword"
- 
-    package 
-    { 
-        "mysql-server":
-            ensure  => present,
-            require => Exec['apt-get update']
-    }
+  $mysqlPassword = "newpassword"
 
-    service 
-    { 
-        "mysql":
-            enable => true,
-            ensure => running,
-            require => Package["mysql-server"],
-    }
+  package
+  {
+      "mysql-server":
+          ensure  => present,
+          require => Exec['apt-get update']
+  }
 
-    # Make sure that any previously setup boxes are gracefully 
-    # transitioned to the new empty root password.
-    exec
-    {
-    	"set-mysql-password":
-            onlyif => "mysqladmin -uroot status",
-            command => "mysqladmin -uroot password $mysqlPassword",
-            require => Service["mysql"],
-    }
+  service
+  {
+      "mysql":
+          enable => true,
+          ensure => running,
+          require => Package["mysql-server"],
+  }
 
-    exec
-    {
-        "create-default-db":
-            unless => "/usr/bin/mysql -uroot -p$mysqlPassword database",
-            command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'create database `database`;'",
-            require => [Service["mysql"], Exec["set-mysql-password"]]
-    }
+  # Make sure that any previously setup boxes are gracefully
+  # transitioned to the new empty root password.
+  exec
+  {
+      "set-mysql-password":
+          onlyif => "mysqladmin -uroot status",
+          command => "mysqladmin -uroot password $mysqlPassword",
+          require => Service["mysql"],
+  }
 
-    exec
-    {
-        "grant-default-db":
-            command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'grant all on `database`.* to `root@localhost`;'",
-            require => [Service["mysql"], Exec["create-default-db"]]
-    }
+  exec
+  {
+      "create-default-db":
+          unless => "/usr/bin/mysql -uroot -p$mysqlPassword database",
+          command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'create database `database`;'",
+          require => [Service["mysql"], Exec["set-mysql-password"]]
+  }
+
+  exec
+  {
+      "grant-default-db":
+          command => "/usr/bin/mysql -uroot -p$mysqlPassword -e 'grant all on `database`.* to `root@localhost`;'",
+          require => [Service["mysql"], Exec["create-default-db"]]
+  }
 }
