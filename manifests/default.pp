9859fd2c0549c5e6b4ed0a5aab6ab416191fe4e6
David Joy <davjoy@gmail.com>
2014-05-19 15:51:59 -0400

    Adding a hostname alias in hosts for "tinbox".  Adding the "use_xdebug" flag.

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,6 +1,15 @@
+# Enable XDebug ("0" | "1")
+$use_xdebug = "1"
+
 # default path
 Exec {
   path => ["/usr/bin", "/bin", "/usr/sbin", "/sbin", "/usr/local/bin", "/usr/local/sbin"]
+}
+
+# create a simple hostname and ip host entry
+host { 'tinbox.nanigans.com':
+  ip => '192.168.56.102',
+  host_aliases => 'tinbox',
 }
 
 include bootstrap
