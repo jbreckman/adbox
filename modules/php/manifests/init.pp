ce6b2481f096f2454d4d741041a4240f6d774748
David Joy <davjoy@gmail.com>
2014-05-19 17:56:28 -0400

    Adding php.ini templates.  The apache2 one includes xdebug support.

--- modules/php/manifests/init.pp
+++ modules/php/manifests/init.pp
@@ -20,4 +20,26 @@ class php {
     ensure => present,
     require => Exec["apt-get update"]
   }
+
+  file
+  {
+    "/etc/php5/apache2/php.ini":
+    ensure  => present,
+    owner   => root, group => root,
+    notify  => Service['apache2'],
+    #source => "/vagrant/puppet/templates/php.ini",
+    content => template('php/php.ini.erb'),
+    require => [Package['php5'], Package['apache2']],
+  }
+
+  file
+  {
+    "/etc/php5/cli/php.ini":
+    ensure  => present,
+    owner   => root, group => root,
+    notify  => Service['apache2'],
+    content => template('php/cli.php.ini.erb'),
+    require => [Package['php5'], Package['php5-cli']],
+  }
+
 }
